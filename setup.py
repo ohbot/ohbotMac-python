fa95800e7f9d928e2bde011c2c81282fa286e3ad
Ohbot <ohbotrobot@gmail.com>
2018-02-25 00:10:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     keywords = ['ohbot', 'robot'],
     classifiers = [],
     install_requires=[
-          'pyserial','comtypes','lxml','playsound',
+          'pyserial','lxml','playsound',
       ],
 )
